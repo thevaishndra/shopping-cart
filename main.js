6c7bebe33a78ab92f92559b052ac3c927de23408
thevaishndra <chandrav306m@gmail.com>
2024-08-22 21:37:27 +0530

    main page

--- main.js
+++ main.js
@@ -28,10 +28,14 @@ let shopItemsData = [{
     desc: "Lorem ipsum dolor sit amet consectetur adipisicing.",
     img: "images/dress.jpg",
 }];
+//if we have data in our basket or if we don't have data we still  want to continue our application instead of breaking
+let basket = JSON.parse(localStorage.getItem("data")) || []
 
 let generateShop = () => {
     return (shop.innerHTML = shopItemsData.map((x) => {
         let {id, name, price, desc, img} = x;
+        //if we find something then store it if not return empty array
+        let search = basket.find((x)=> x.id === id) || []
         return `
         <div id=product-id-${id}" class="item">
             <img width="219" height="290" src="${img}"/>
@@ -42,7 +46,7 @@ let generateShop = () => {
                     <h2>$ ${price}</h2>
                     <div class="buttons">
                         <i onclick="decrement(${id})" class="bi bi-dash-lg"></i>
-                        <div id=${id} class="quantity">0</div>
+                        <div id=${id} class="quantity">${search.item === undefined? 0:search.item}</div>
                         <i onclick="increment(${id})" class="bi bi-plus-lg"></i>
                     </div>
                 </div>
@@ -56,10 +60,44 @@ generateShop();
 
 let increment = (id) => {
     let selectedItem = id;
-    console.log(selectedItem.id);
+    let search = basket.find((x) => x.id === selectedItem.id);
+//if we have not added any item in basket then we will push
+    if (search === undefined){
+        basket.push({
+            id: selectedItem.id,
+            item: 1,
+        });
+    }//but if the item already exists in the basket we will just increment it
+    else {
+        search.item += 1;
+    }
+    localStorage.setItem("data", JSON.stringify(basket));
+    // console.log(basket);
+    update(selectedItem.id);
 }
 let decrement = (id) => {
     let selectedItem = id;
-    console.log(selectedItem.id);
+    let search = basket.find((x) => x.id === selectedItem.id);
+
+    if (search.item === 0) return;
+    else {
+        search.item -= 1;
+    }
+    localStorage.setItem("data", JSON.stringify(basket));
+    // console.log(basket);
+    update(selectedItem.id);
 }
-let update = () => {}
+let update = (id) => {
+   let search = basket.find((x) => x.id === id);
+//    console.log(search);
+   document.getElementById(id).innerHTML = search.item;
+   calculation();
+};
+
+let calculation =() => {//we have to calculate all the items and make it appear on top right
+    let cartIcon = document.getElementById("cartAmount");
+    //one of them is previous no. in x and y
+    cartIcon.innerHTML = basket.map((x) => x.item).reduce((x, y) => x + y, 0)
+}
+
+calculation();//everytime the application loads this will run
